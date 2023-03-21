1dd1f63420ffa96683e7313e40e9f7cda1ce2285
Haerul Taka <haerultaka255@gmail.com>
2023-03-21 20:12:29 +0700

    menambahkan code javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,17 @@
-function myFunction() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
-}
+// Menu Toolbar
+const navbarNav = document.querySelector('.navbar-nav');
+
+// click
+document.querySelector('#menu').onclick = () => {
+  navbarNav.classList.toggle('active');
+};
+
+// menghilangkan toolbar
+const menu = document.querySelector('#menu');
+document.addEventListener('click', function(e){
+  if(!menu.contains(e.target) && !navbarNav.contains(e.target)){
+    navbarNav.classList.remove('active');
+  }
+
+});
+
